af52d35e068dd29807d8667346800811c92326a6
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-03-28 10:29:39 +0100

    Update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,32 @@
-
+import React from "react"
+import Back from "../common/Back";
+import img6 from "../images/6.jpg";
+import "./contact.css"
+///boomerang email
 
 const Contact = () => {
     return (
         <div>
-            back 
-            contact info 
+            <section className="contact mb">
+            <Back
+          name="Contact us"
+          title="Get Helps & Friendly Support"
+          cover={img6}
+        />
+        <div className="container">
+            <form className="shadow">
+                <h4>Fill in the form</h4>
+                <div>
+                    <input type="text" placeholder="name" />
+                    <input type="text" placeholder="email" />
+                </div>
+                <input type="text" placeholder="subject" />
+                <textarea cols='30' rows='10'></textarea>
+                <button className="btn-search cross-logo ">Submit Request </button>
+            </form>
+        </div>
+
+            </section>
         </div>
     )
 }
